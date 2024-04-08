a8b1dfcca2ac1297bfd5b745d774f48ec977b16b
Chendi Qian <1049653012@qq.com>
2024-04-08 20:58:57 +0200

    use faster method

--- data/utils.py
+++ data/utils.py
@@ -5,7 +5,7 @@ from torch_geometric.data import Data, Batch
 import numpy as np
 from solver.linprog_ip import _ip_hsd
 # from scipy.optimize import linprog
-from scipy.sparse import block_diag
+# from scipy.sparse import block_diag
 
 
 def args_set_bool(args: Dict):
@@ -52,12 +52,40 @@ def feasible_start_point(graph: Data, keep_lp=False):
     return graph
 
 
+# def collate_fn_lp(graphs: List[Data]):
+#     proj_matrices = [g.pop('proj_matrix').numpy().reshape(g.pop('proj_mat_shape').tolist()) for g in graphs]
+#     new_batch = Batch.from_data_list(graphs)
+#     proj_matrices = block_diag(proj_matrices)
+#     proj_matrices = SparseTensor.from_scipy(proj_matrices)
+#     new_batch.proj_matrix = proj_matrices
+#     return new_batch
+
+
 def collate_fn_lp(graphs: List[Data]):
-    proj_matrices = [g.pop('proj_matrix').numpy().reshape(g.pop('proj_mat_shape').tolist()) for g in graphs]
+    cumsum_row = 0
+    cumsum_col = 0
+
+    row_indices = []
+    col_indices = []
+    vals = []
+
+    for g in graphs:
+        vals.append(g.pop('proj_matrix'))
+        nrows, ncols = g.pop('proj_mat_shape').tolist()
+        row_idx = np.arange(cumsum_row, cumsum_row + nrows).repeat(ncols)
+        cumsum_row += nrows
+        col_idx = np.tile(np.arange(cumsum_col, cumsum_col + ncols), nrows)
+        cumsum_col += ncols
+        row_indices.append(row_idx)
+        col_indices.append(col_idx)
+
+    row_indices = torch.from_numpy(np.concatenate(row_indices, axis=0))
+    col_indices = torch.from_numpy(np.concatenate(col_indices, axis=0))
+    vals = torch.cat(vals, dim=0)
+
+    proj_matrix = SparseTensor(row=row_indices, col=col_indices, value=vals, is_sorted=True, trust_data=True)
     new_batch = Batch.from_data_list(graphs)
-    proj_matrices = block_diag(proj_matrices)
-    proj_matrices = SparseTensor.from_scipy(proj_matrices)
-    new_batch.proj_matrix = proj_matrices
+    new_batch.proj_matrix = proj_matrix
     return new_batch
 
 

33d3d41efa65cbb09d9bc6a5f2d3991ae57f1159
Chendi Qian <1049653012@qq.com>
2024-05-18 14:59:19 +0200

    add util func

--- data/utils.py
+++ data/utils.py
@@ -1,9 +1,11 @@
 from typing import Dict, List
+import time
 import torch
 from torch_sparse import SparseTensor
 from torch_scatter import scatter_min
 from torch_geometric.data import Data, Batch
 import numpy as np
+import seaborn as sns
 
 
 def args_set_bool(args: Dict):
@@ -87,3 +89,25 @@ def batch_line_search(x: torch.Tensor, direction: torch.Tensor, batch: torch.Ten
     alpha = scatter_min(alpha, batch)[0]
     alpha = alpha[batch]
     return alpha
+
+
+def sync_timer():
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+    return time.time()
+
+
+def gaussian_filter(xs_grid, xs_data, ys_data, sigma):
+    weights = np.exp(-(xs_data - xs_grid[:, None])**2 / (2 * sigma**2))
+    return np.sum(weights * ys_data, 1) / np.sum(weights, 1)
+
+
+def gaussian_filter_bt(xs_grid, xs_data, ys_data, sigma, n_boot=2000):
+    bootstrap_res = sns.algorithms.bootstrap(
+            np.column_stack((xs_data, ys_data)),
+            func=lambda x: gaussian_filter(xs_grid, x[:, 0], x[:, 1], sigma=sigma),
+            n_boot=n_boot)
+
+    mean = bootstrap_res.mean(0)
+    ci = sns.utils.ci(bootstrap_res, axis=0)
+    return mean, ci

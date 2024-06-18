90b9c06907f91c7e2f54b705bca17a8468a02078
Chendi Qian <1049653012@qq.com>
2024-06-18 12:12:37 +0200

    change log name

--- run.py
+++ run.py
@@ -59,8 +59,8 @@ if __name__ == '__main__':
     if args.ckpt:
         if not os.path.isdir('logs'):
             os.mkdir('logs')
-        exist_runs = [d for d in os.listdir('logs') if d.startswith('exp')]
-        log_folder_name = f'logs/exp{len(exist_runs)}'
+        exist_runs = [d for d in os.listdir('logs') if d.startswith(args.wandbname)]
+        log_folder_name = f'logs/{args.wandbname}exp{len(exist_runs)}'
         os.mkdir(log_folder_name)
         with open(os.path.join(log_folder_name, 'config.yaml'), 'w') as outfile:
             yaml.dump(vars(args), outfile, default_flow_style=False)

c9dd869f908bf4a526fb446e1b6b769a6e71f67c
Chendi Qian <1049653012@qq.com>
2024-07-22 10:04:10 +0200

    add float16

--- trainer.py
+++ trainer.py
@@ -3,17 +3,24 @@ from torch_geometric.utils import to_dense_batch
 from torch_scatter import scatter
 from torch_sparse import spmm
 
+if torch.cuda.is_available():
+    device = 'cuda'
+    scaler = torch.cuda.amp.GradScaler()
+    autocast = torch.cuda.amp.autocast
+else:
+    scalar = torch.cpu.amp.GradScaler()
+    autocast = torch.cpu.amp.autocast
+    device = 'cpu'
+
 
 class Trainer:
     def __init__(self,
-                 device,
                  loss_type,
                  loss_lambda):
         self.best_val_loss = 1.e8
         self.best_cos_sim = 1.e8
         self.best_objgap = 1.e8
         self.patience = 0
-        self.device = device
         self.loss_type = loss_type
         self.cos_metric = torch.nn.CosineEmbeddingLoss(reduction='none')
         if loss_type == 'l2':
@@ -28,29 +35,33 @@ class Trainer:
 
     def train(self, dataloader, model, optimizer):
         model.train()
-        optimizer.zero_grad()
 
         train_losses = 0.
         cos_sims = 0.
         num_graphs = 0
         for i, data in enumerate(dataloader):
-            data = data.to(self.device)
-            pred, label = model(data)  # nnodes x steps
-            loss = self.get_loss(pred, label, data['vals'].batch)
-            cos_sim = self.get_cos_sim(pred, label, data['vals'].batch)
+            data = data.to(device)
+            optimizer.zero_grad()
 
-            train_losses += loss.detach() * data.num_graphs
-            cos_sims += cos_sim.detach() * data.num_graphs
-            num_graphs += data.num_graphs
+            with autocast(dtype=torch.float16):
+                pred, label = model(data)  # nnodes x steps
+                loss = self.get_loss(pred, label, data['vals'].batch)
+                cos_sim = self.get_cos_sim(pred, label, data['vals'].batch)
 
-            # use both L2 loss and Cos similarity loss
-            loss = loss + self.loss_lambda * cos_sim
-            loss.backward()
+                train_losses += loss.detach() * data.num_graphs
+                cos_sims += cos_sim.detach() * data.num_graphs
+                num_graphs += data.num_graphs
+
+                # use both L2 loss and Cos similarity loss
+                loss = loss + self.loss_lambda * cos_sim
+
+            scaler.scale(loss).backward()
+            scaler.unscale_(optimizer)
             torch.nn.utils.clip_grad_norm_(model.parameters(),
                                            max_norm=1.0,
                                            error_if_nonfinite=True)
-            optimizer.step()
-            optimizer.zero_grad()
+            scaler.step(optimizer)
+            scaler.update()
 
         return train_losses.item() / num_graphs, cos_sims.item() / num_graphs
 
@@ -63,17 +74,18 @@ class Trainer:
         num_graphs = 0
         obj_gaps = []
         for i, data in enumerate(dataloader):
-            data = data.to(self.device)
-            pred, label = model(data)
-            loss = self.get_loss(pred, label, data['vals'].batch)
-            cos_sim = self.get_cos_sim(pred, label, data['vals'].batch)
+            data = data.to(device)
+            with autocast(dtype=torch.float16):
+                pred, label = model(data)
+                loss = self.get_loss(pred, label, data['vals'].batch)
+                cos_sim = self.get_cos_sim(pred, label, data['vals'].batch)
 
-            val_losses += loss * data.num_graphs
-            cos_sims += cos_sim * data.num_graphs
-            num_graphs += data.num_graphs
+                val_losses += loss * data.num_graphs
+                cos_sims += cos_sim * data.num_graphs
+                num_graphs += data.num_graphs
 
-            obj_gap, *_ = model.evaluation(data)
-            obj_gaps.append(obj_gap)
+                obj_gap, *_ = model.evaluation(data)
+                obj_gaps.append(obj_gap)
 
         objs = torch.cat(obj_gaps, dim=0).mean().item()
         return val_losses.item() / num_graphs, cos_sims.item() / num_graphs, objs
@@ -100,8 +112,9 @@ class Trainer:
         violations = 0.
         num_graphs = 0
         for i, data in enumerate(dataloader):
-            data = data.to(self.device)
-            pred, _ = model(data)
+            data = data.to(device)
+            with autocast(dtype=torch.float16):
+                pred, _ = model(data)
             Ax_minus_b = spmm(data['cons', 'to', 'vals'].edge_index,
                               data['cons', 'to', 'vals'].edge_attr.squeeze(),
                               data['cons'].num_nodes, data['vals'].num_nodes, pred).squeeze() - data.b

ef939c5449793609d20d4d6064a47d5ec57fb062
Chendi Qian <1049653012@qq.com>
2024-07-24 20:41:10 +0200

    fix bug

--- utils/data.py
+++ utils/data.py
@@ -8,7 +8,7 @@ def l1_normalize(x: torch.Tensor, dim=0):
 
 
 def batch_l1_normalize(x: torch.Tensor, batch: torch.Tensor, dim=0):
-    maxs = scatter(x, batch, dim=dim, reduce='max') + 1.e-7
+    maxs = scatter(x.abs(), batch, dim=dim, reduce='max') + 1.e-7
     maxs = maxs[batch]
     x /= maxs
     return x

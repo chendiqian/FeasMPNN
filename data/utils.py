fd104d8be2cc6697b86972384a079f6bc803fccb
Chendi Qian <1049653012@qq.com>
2024-07-29 12:49:55 +0200

    not inplace normalize

--- data/utils.py
+++ data/utils.py
@@ -31,27 +31,14 @@ def gaussian_filter_bt(xs_grid, xs_data, ys_data, sigma, n_boot=2000):
 
 
 def l1_normalize(x: torch.Tensor, dim=0):
-    x /= x.abs().max(dim=dim, keepdims=True).values + 1.e-7
+    x = x / x.abs().max(dim=dim, keepdims=True).values + 1.e-7
     return x
 
 
 def batch_l1_normalize(x: torch.Tensor, batch: torch.Tensor, dim=0):
-    """
-    Theoretical this is the right thing to do, but the above works much better
-
-    Parameters
-    ----------
-    x
-    batch
-    dim
-
-    Returns
-    -------
-
-    """
     maxs = scatter(x.abs(), batch, dim=dim, reduce='max') + 1.e-7
     maxs = maxs[batch]
-    x /= maxs
+    x = x / maxs
     return x
 
 

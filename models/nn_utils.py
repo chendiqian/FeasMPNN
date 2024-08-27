5de4f1b62094e50d6e2bdde398479a234ca2b648
Chendi Qian <1049653012@qq.com>
2024-08-27 16:46:37 +0200

    add encoder

--- models/nn_utils.py
+++ models/nn_utils.py
@@ -21,3 +21,18 @@ class LogEncoder(torch.nn.Module):
 
     def forward(self, x):
         return torch.log(1. / (x + self.bias))
+
+
+class SQRTEncoder:
+    def __init__(self):
+        pass
+
+    def sqrt(self, x):
+        sign = torch.sign(x)
+        x = sign * torch.sqrt(torch.abs(x))
+        return x
+
+    def sqr(self, x):
+        sign = torch.sign(x)
+        x = sign * (x ** 2.)
+        return x

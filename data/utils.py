a5d3c971f020505c777a47218ae756fed2d4bc97
Chendi Qian <1049653012@qq.com>
2024-10-28 14:50:42 +0100

    minor fix

--- data/utils.py
+++ data/utils.py
@@ -47,8 +47,9 @@ def save_run_config(args: DictConfig):
     if args.ckpt:
         if not os.path.isdir('logs'):
             os.mkdir('logs')
-        exist_runs = [d for d in os.listdir('logs') if d.startswith(args.wandbname)]
-        log_folder_name = f'logs/{args.wandbname}exp{len(exist_runs)}'
+        prefix = os.path.join(args.wandb.project, args.wandb.name)
+        exist_runs = [d for d in os.listdir('logs') if d.startswith(prefix)]
+        log_folder_name = f'logs/{prefix}_exp{len(exist_runs)}'
         os.mkdir(log_folder_name)
         # with open(os.path.join(log_folder_name, 'config.yaml'), 'w') as outfile:
         #     yaml.dump(vars(args), outfile, default_flow_style=False)

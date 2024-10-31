b585287ac25ad5a930dad4892ebfbb56de1ab894
Chendi Qian <1049653012@qq.com>
2024-10-31 15:38:43 +0100

    add gurobi solver

--- evaluate_solver.py
+++ evaluate_solver.py
@@ -7,6 +7,7 @@ from omegaconf import DictConfig, OmegaConf
 from data.dataset import LPDataset
 from data.utils import sync_timer, recover_qp_from_data
 from qpsolvers import solve_qp
+import gurobipy as gp
 
 
 @hydra.main(version_base=None, config_path='./config', config_name="evaluate")
@@ -21,6 +22,7 @@ def main(args: DictConfig):
 
     cvxopt_time = []
     osqp_time = []
+    gurobi_time = []
 
     pbar = tqdm(dataset)
     for data in pbar:
@@ -36,8 +38,24 @@ def main(args: DictConfig):
         end_t = sync_timer()
         osqp_time.append(end_t - start_t)
 
+        # explicitly make it convex
+        # otherwise you might have weird behavior
+        # Continuous model is non-convex -- solving as a MIP
+        P = P + np.eye(P.shape[0]) * 0.001
+
+        m = gp.Model("qp")
+        m.Params.LogToConsole = 0  # no outputs
+        x = m.addMVar(A.shape[1])
+        m.setObjective(0.5 * x @ P @ x + (x * q).sum())
+        m.addConstr(A @ x == b)
+        start_t = sync_timer()
+        m.optimize()
+        end_t = sync_timer()
+        gurobi_time.append(end_t - start_t)
+
         stat_dict = {'cvxopt': cvxopt_time[-1],
-                     'osqp': osqp_time[-1]}
+                     'osqp': osqp_time[-1],
+                     'gurobi': gurobi_time[-1]}
 
         wandb.log(stat_dict)
         pbar.set_postfix(stat_dict)
@@ -45,7 +63,9 @@ def main(args: DictConfig):
     stat_dict = {"cvxopt_mean": np.mean(cvxopt_time),
                  "cvxopt_std": np.std(cvxopt_time),
                  "osqp_mean": np.mean(osqp_time),
-                 "osqp_std": np.std(osqp_time)}
+                 "osqp_std": np.std(osqp_time),
+                 "gurobi_mean": np.mean(gurobi_time),
+                 "gurobi_std": np.std(gurobi_time)}
 
     wandb.log(stat_dict)
 

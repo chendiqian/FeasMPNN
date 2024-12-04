7bce3a42249d6459686045cb57425f55bfcf0964
Chendi Qian <1049653012@qq.com>
2024-12-04 15:12:42 +0100

    add string

--- evaluate_fixstep_ipm.py
+++ evaluate_fixstep_ipm.py
@@ -103,7 +103,15 @@ def main(args: DictConfig):
                  "gnn_violation_mean": np.mean(gnn_violations),
                  "gnn_violation_std": np.std(gnn_violations),
                  "gnn_time_mean": np.mean(gnn_times),
-                 "gnn_time_std": np.std(gnn_times)}
+                 "gnn_time_std": np.std(gnn_times),
+                 # for latex convenience
+                 "percent_obj_string": f'{np.mean(best_gnn_obj) * 100:.3f}'
+                                       f'\scriptsize$\pm${np.std(best_gnn_obj) * 100:.3f}',
+                 "gnn_time_string": f'{np.mean(gnn_times):.3f}'
+                                    f'\scriptsize$\pm${np.std(gnn_times):.3f}',
+                 "vio_string": f'{np.mean(gnn_violations):.3f}'
+                               f'\scriptsize$\pm${np.std(gnn_violations):.3f}'
+                 }
 
     wandb.log(stat_dict)
 
